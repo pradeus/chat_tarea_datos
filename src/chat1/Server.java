e65f082bda7a56f4e85cd04b80a6a35f21f4b972
pradeus <47864316+pradeus@users.noreply.github.com>
2020-09-25 15:15:58 -0600

    Update Server.java

--- src/chat1/Server.java
+++ src/chat1/Server.java
@@ -23,7 +23,7 @@ public class Server implements Runnable{//esto es para hacer los threads
     private int puerto;
     
 //esta funcion genera el thread del server, recibe la lista que será el display del chat
-    public Server(DefaultTableModel modelo_server, int puerto_APP) {
+    public Server(DefaultTableModel modelo_server, int puerto_APP) {//uso defaulttablemodel porque use una lista para que tire el nombre-mensaje y solo use addrow para que se vea mejor
         modelo = modelo_server;
         puerto = puerto_APP;
         Thread thread1 = new Thread( this);
@@ -31,7 +31,7 @@ public class Server implements Runnable{//esto es para hacer los threads
     }
     
     
-    public void Update(int puerto_APP){
+    public void Update(int puerto_APP){//funcion para actualizar el port
         puerto = puerto_APP;
         
     }
@@ -39,19 +39,19 @@ public class Server implements Runnable{//esto es para hacer los threads
     @Override
     public void run() {
         try{
-            //creando los sockets del server
+            //creando los sockets del server *** aquí se presenta el error
             ServerSocket Servidor = new ServerSocket(puerto);
             while(true){
-                try (Socket cliente = Servidor.accept()) {
+                try (Socket cliente = Servidor.accept()) {//aquí actúa como receptor 
                     DataInputStream mensaje_entrante = new DataInputStream(cliente.getInputStream());
-                    mensaje = mensaje_entrante.readUTF().split("°");
-                    modelo.addRow(new Object[]{mensaje[0],mensaje[1]});
-                    cliente.close();
+                    mensaje = mensaje_entrante.readUTF().split("°");//aqui se separa el mensaje en la tabla gracias a compresor
+                    modelo.addRow(new Object[]{mensaje[0],mensaje[1]});//se agregaría el mensaje a la tabla
+                    cliente.close();//cerramos el socket
                 }
                 
             }
         }catch(IOException e){
-            System.out.println("F por el socket");
+            System.out.println("F por el socket");//aquí termina el error al abrir el segundo programa
         }
     }
 }
